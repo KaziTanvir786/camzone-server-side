1767194f23b4ab066b187727c689dec626d92b55
KaziTanvir786 <kazitanvir786@gmail.com>
2021-11-13 06:23:55 +0600

    delete order API added

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         })
+
+        //DELETE order API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
