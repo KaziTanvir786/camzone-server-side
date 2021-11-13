09f1ebfdd7651d7212b6fc0735c415106b528d1d
KaziTanvir786 <kazitanvir786@gmail.com>
2021-11-13 12:06:32 +0600

    Update order API added

--- index.js
+++ index.js
@@ -127,6 +127,21 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
+
+        //UPDATE order API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    status: 'Confirmed'
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
