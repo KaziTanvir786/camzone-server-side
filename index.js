32341c2241f7063ace1c26320238a9e15d71009e
KaziTanvir786 <kazitanvir786@gmail.com>
2021-11-13 11:20:40 +0600

    Make admin API added

--- index.js
+++ index.js
@@ -118,6 +118,15 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+
+        //Make admin API
+        app.put('users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
     }
     finally {
         // await client.close();
